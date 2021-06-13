96844b0b4de0c0fe441a39293b970e39d6913118
LukSiman <simanavicius.lukas@gmail.com>
2021-06-12 21:22:25 -0700

    added five round auto

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-
 function computerPlay(){
     let result;
     let random = Math.floor(Math.random() * 3);
@@ -17,29 +14,47 @@ function computerPlay(){
     }
 }
 
+function game(){
+    let score = 0;
+    for(i = 1; i <= 5; ++i){
+        let computerSelection = computerPlay();
+        score += playRound(playerSelection, computerSelection);
+    }
+    if(score === 0){
+        console.log("Final result is a draw!");
+    } else if(score < 0){
+        console.log("You lost, computer wins!");
+    } else {
+        console.log("You won, computer lost!");
+    }
+
+}
+
 function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
 
     if(player === computer){
-        return "Draw!";
+        console.log("Draw!");
+        return 0;
     } else if(player === "rock" && computer === "paper"){
-        return "You Lose! Paper beats Rock";
+        console.log("You Lose! Paper beats Rock");
+        return -1;
 
     } else if(player === "scissors" && computer === "rock"){
-        return "You Lose! Rock beats Scissors";
+        console.log("You Lose! Rock beats Scissors");
+        return -1;
 
     } else if(player === "paper" && computer === "scissors"){
-        return "You Lose! Scissors beats Paper";
+        console.log("You Lose! Scissors beats Paper");
+        return -1;
         
     } else {
         player = player.slice(0,1).toUpperCase() + player.slice(1);
         computer = computer.slice(0,1).toUpperCase() + computer.slice(1);
-       
-        return `You Win! ${player} beats ${computer}`;
+        console.log(`You Win! ${player} beats ${computer}`);
+        return 1;
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-
-//const playerSelection = prompt("Enter your play! Rock, paper or scissors.");
+const playerSelection = "rock";
