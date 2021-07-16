bfa2d0d4292681f0ca1470f8d98ac8c72f6f9d9e
LukSiman <simanavicius.lukas@gmail.com>
2021-07-15 17:52:07 -0700

    adding css styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-function computerPlay(){
+function computerPlay() {
     let result;
     let random = Math.floor(Math.random() * 3);
-    switch(random){
+    switch (random) {
         case 0:
             return "Rock";
             break;
@@ -27,16 +27,16 @@ function computerPlay(){
     }
 }*/
 
-function game(button){
+function game(button) {
     let score = 0;
-    for(i = 1; i <= 5; ++i){
+    for (i = 1; i <= 5; ++i) {
         let computerSelection = computerPlay();
         let playerSelection = button.id;
         score += playRound(playerSelection, computerSelection);
     }
-    if(score === 0){
+    if (score === 0) {
         console.log("Final result is a draw!");
-    } else if(score < 0){
+    } else if (score < 0) {
         console.log("You lost, computer wins!");
     } else {
         console.log("You won, computer lost!");
@@ -44,29 +44,29 @@ function game(button){
 
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
 
     const result = document.querySelector('#result');
 
-    if(player === computer){
+    if (player === computer) {
         result.textContent = "Draw!";
-    } else if(player === "rock" && computer === "paper"){
+    } else if (player === "rock" && computer === "paper") {
         result.textContent = "You Lose! Paper beats Rock!";
         computerScore++;
 
-    } else if(player === "scissors" && computer === "rock"){
+    } else if (player === "scissors" && computer === "rock") {
         result.textContent = "You Lose! Rock beats Scissors!";
         computerScore++;
 
-    } else if(player === "paper" && computer === "scissors"){
+    } else if (player === "paper" && computer === "scissors") {
         result.textContent = "You Lose! Scissors beats Paper!";
         computerScore++;
 
     } else {
-        player = player.slice(0,1).toUpperCase() + player.slice(1);
-        computer = computer.slice(0,1).toUpperCase() + computer.slice(1);
+        player = player.slice(0, 1).toUpperCase() + player.slice(1);
+        computer = computer.slice(0, 1).toUpperCase() + computer.slice(1);
         result.textContent = `You Win! ${player} beats ${computer}!`;
         playerScore++;
     }
@@ -80,7 +80,7 @@ const buttons = document.querySelectorAll('button');
 
 const game_score = document.querySelector('#score');
 const final_result = document.createElement('p');
-final_result.setAttribute('style', 'font-weight: bold; font-size: 30px');
+
 
 const player_score = document.querySelector('#player_score');
 const player_result = document.createElement('p');
@@ -93,48 +93,50 @@ buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playRound(button.id, computerPlay());
 
-        player_result.textContent = romanNumberConverter(playerScore);        
+        player_result.textContent = romanNumberConverter(playerScore);
         player_score.appendChild(player_result);
 
         computer_result.textContent = romanNumberConverter(computerScore);
-        computer_score.appendChild(computer_result);      
+        computer_score.appendChild(computer_result);
 
-        if(round_over){
+        if (round_over) {
             game_score.removeChild(final_result);
             round_over = 0;
         }
 
-        if(computerScore === 5){
-            final_result.textContent = "You lost, computer wins!";
+        if (computerScore === 5) {
+            final_result.setAttribute('id', 'computer_wins');
+            final_result.textContent = "BEEP BOOP, computer wins!";
             game_score.appendChild(final_result);
             computerScore = 0;
             playerScore = 0;
             round_over = 1;
-        } else if(playerScore === 5){
-            final_result.textContent = "You won, computer lost!";
+        } else if (playerScore === 5) {
+            final_result.setAttribute('id', 'human_wins');
+            final_result.textContent = "You won, humanity saved!";
             game_score.appendChild(final_result);
             computerScore = 0;
             playerScore = 0;
             round_over = 1;
-        }    
-    }); 
+        }
+    });
 });
 
 
-function romanNumberConverter(arabicNumber){
+function romanNumberConverter(arabicNumber) {
     let roman = "";
-    const romanNumList = {V:5, IV:4, I:1};
+    const romanNumList = { V: 5, IV: 4, I: 1 };
     let a;
 
-    for(let key in romanNumList){
+    for (let key in romanNumList) {
         a = Math.floor(arabicNumber / romanNumList[key]);
-        if(a >= 0){
-            for(let i = 0; i < a; i++){
-            roman += key;
+        if (a >= 0) {
+            for (let i = 0; i < a; i++) {
+                roman += key;
             }
         }
         arabicNumber = arabicNumber % romanNumList[key];
-      }
+    }
 
     return roman;
 }
